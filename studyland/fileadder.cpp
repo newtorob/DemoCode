93610d7f88e66fbaab1837ff2188415485236b9e
Robert Newton <newtorob@oregonstate.edu>
2016-12-26 01:59:11 -0500

    Added a file

---
+++ studyland/fileadder.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string fileName;
+  ifstream  inputFile;
+  ofstream outputFile;
+
+  cout << "Hello, what is the name of the file you want to open?" << endl;
+  cin >> fileName;
+
+  outputFile.open
+
+}
