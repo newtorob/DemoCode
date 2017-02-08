3cf3376fb80c83564655e1d0982f078208ab4b2e
Robert Newton <newtorob@oregonstate.edu>
2017-02-08 11:51:14 -0500

    java changes

---
+++ JavaPractice/LovePoem.java
@@ -0,0 +1,35 @@
+class LovePoem {
+        /*generally you want the classes to have a capitlization 
+        this avoids confusion when calling it later
+        if need be of course
+
+         also just a note, download git, learn github and 
+         put everything up on it, literally everything
+        this will be important when you are looking for a 
+        job. If you need any help with this let me know
+        I have given conference talks to a lot of people
+        about github and the importance of it.
+        */
+       public static void main(String[] args) {
+          System.out.println("\n");
+          System.out.println("Roses are Red\n");
+          System.out.println("Violets are Blue\n");
+          System.out.println("You Run fast\n");
+          System.out.println("So, I can't catch you!\n");
+          System.out.println("Happy Valentines Day!\n");
+       }
+          /*system out is essentially print from
+            what I can gather, if you look at 
+            JavaScript, it would look like 
+            console.log, or c++ it would be 
+            cout (console out). 
+          */
+
+          /*the  \n is called an escape sequence,
+          essentially, it is a way to break into a 
+          new line, this organizes the program a little
+          better and makes reading it in the console
+          a lot better
+          */
+       
+    }
