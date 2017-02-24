bc065f558936c4c8e7ff5bcf57ddccdd25efcf61
Robert Newton <newtorob@oregonstate.edu>
2017-02-24 01:00:20 -0500

    Java practice

---
+++ JavaPractice/body.java
@@ -0,0 +1,5 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.print("Text to be printed");
+        }
+}
---
+++ JavaPractice/comm.java
@@ -0,0 +1,6 @@
+System.out.print("hello world");
+System.out.print(" and all the people of the world.");
+//print more text to the same line.
+//printing out both lines. 
+
+System.out.println("First\nSecond");
