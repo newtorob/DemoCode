7f6391c2858e2273d2fcf3be184713face463674
Robert Newton <newtorob@oregonstate.edu>
2017-02-27 14:59:56 -0500

    Java

---
+++ JavaPractice/addition.java
@@ -0,0 +1,16 @@
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int a = 1337;
+        int b = 42;
+        int result; // Fix 
+        
+        result = a + b;
+
+        String toPrint = a + " + " + b + " = " + result;
+        System.out.println(toPrint);
+
+    }
+
+}
---
+++ JavaPractice/multiplication.java
@@ -0,0 +1,16 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 1337;
+        int b = 42;
+        int result;
+
+        // Program your solution here. Remember to use variables a and b!
+        result = a * b;
+        
+        System.out.println(result);
+        
+    }
+
+}
